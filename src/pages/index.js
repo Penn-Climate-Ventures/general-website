dca9eb7bf5ebd8938df30e952d20d5d4da00abeb
ryoma <84463842+hryoma@users.noreply.github.com>
2021-06-02 18:44:23 -0400

    Added more margins to side

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,22 +22,6 @@ import {
   OXFORD_BLUE
 } from "../utils/constants"
 
-const ProblemStatement = () => (
-  <div className="grid" css={`display: grid; grid-auto-flow: column`}>
-    <div className="col">
-      <p>Imagine a campus where students line up to work for the biggest moveers in climate innovation.</p>
-      <p>Imagine a campus where sustainability and climate analyses are integrated into our course plans, just like intro statistics or calulus.</p>
-      <p css={`font-weight: 700;`}>Imagine a campus where climate is just... common sense.</p>
-    </div>
-    <div className="col" css={`color: blue; border: 1px solid blue; border-radius: 10px; text-align: center; padding: 10px; align-items: center; display: flex;`}>
-      <div>
-        <p>That's what we're building.</p>
-        <p>Let's push Penn into Climate.</p>
-      </div>
-    </div>
-  </div>
-)
-
 
 const OrientationAndNewsGrid = s.div`
   display: grid;
@@ -52,7 +36,7 @@ const IndexPage = () => {
   const sectionRef = useRef(null)
   const scrollTo = () => sectionRef && sectionRef.current.scrollIntoView()
   return (
-    <div css={`margin: auto 1rem;`}>
+    <div css={`margin: auto 3rem;`}>
       <Helmet title="Penn Climate Ventures Prize" defer={false} />
       <Navbar />
       <Imagine />
