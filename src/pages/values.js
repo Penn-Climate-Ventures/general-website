01305ad7940d9296f5dd8b888229bfd612233e6d
yianhsieh <yh667@seas.upenn.edu>
2021-08-09 19:41:39 -0400

    Update values.js

--- src/pages/values.js
+++ src/pages/values.js
@@ -36,67 +36,49 @@ export default class ClassPage extends Component {
         <PageTitle>Our Values</PageTitle>
 
         <TextP>
-          At Penn Climate Ventures, all of our actions ought to be centered around sustainability.
-          Our central thesis is that we can act to combat the climate crisis and larger human-caused
-          issues in our environment, and we seek to make all activities
+          As a student-led group at Penn, PCV’s mission is to support Penn students in catalyzing climate action. 
+          Our long-term vision is to build a climate tech career recruiting pipeline at Penn, comparable to those 
+          of consulting and investment banking that currently exist. Our central thesis is that we CAN act to combat 
+          the climate crisis and larger anthropogenic issues in our environment. 
         </TextP>
 
         <TextP>
-          At Penn Climate Ventures, we recognize that the Earth is facing a crisis unlike ever
-          before. Climate change threatens the lives of billions of people in all socioeconomic
-          status. Extreme weather conditions, depleting resources, and natural disasters are a few
-          of the long lasting effects. These problems are exacerbated on impoverished people,
-          communities, and nations. Not just human lives are at risk but the quality and experiences
-          of life. Our ecosystem and biodiversity have intangible value that may very well
-          disappear. Responding to climate change requires movements across economic, social, and
-          political boundaries. And one of the least lacking movements is economics.
-        </TextP>
+        Responding to climate change requires movements across economic, social, and political boundaries. 
+        We aim to make climate and business education more accessible, connect Penn talent to industry needs, 
+        and foster innovation and entrepreneurship at the undergraduate and graduate levels. 
+        Our organization operates on these guiding principles:
 
-        <TextP>
-          Penn Climate Ventures aims to build business oriented climate leaders to solve this
-          crisis. To help achieve this goal, we will base our decisions on a list of values. They
-          are not organized on any level of importance and do not act as dogma. Rather they will
-          act as guidance as we move forward.
         </TextP>
 
         <ul>
           <li>
-            <b>All activities made internally should do no harm to the environment.</b> And whenever
-            possible should decrease our environmental impact. Merchandise should be bought from
-            sustainable suppliers. Catering and food orders should be from sustainable sources.
-            Funded trips should be taken responsibly with the end goal of becoming more sustainably
-            knowledgeable or having a net positive impact on the environment.
+            <b> 
+            All activities and events should be environmentally mindful </b> and whenever possible 
+            should decrease our environmental impact. All spending including speakers, merchandise, 
+            catering, and funded trips should be taken responsibly with the end goal of becoming more 
+            sustainably knowledgeable or having a net positive impact on the environment.
           </li>
           <li>
-            <b>Events should be environmentally mindful.</b> Fellowship working on projects should
-            be under the basis that they will make a positive impact and prepare students for these
-            opportunities. And lectures/workshops should work to achieve climate leaders.
+            <b>Successful communities are part of a sustainable environment.</b> We are an integral 
+            part of the community engaged with the Penn students, the Penn administration, and Philadelphia, 
+            and our patrons globally. We are responsible to engage these groups in mutually-beneficial, 
+            non-invasive manners.
           </li>
           <li>
-            <b>Successful communities are part of a sustainable environment.</b> We should consider
-            ourselves to be an integral part of the community engaged with the Penn students, the
-            Penn administration, and the Philadelphia city. We are responsible to engage these
-            groups with their benefit in mind.
+            <b>We seek to be diverse.</b> We hope to represent all of Penn schools and students from 
+            various backgrounds. We aim to foster cross-collaboration across these schools 
+            and build deeper relationships.
           </li>
           <li>
-            <b>We seek to be diverse.</b> We hope to represent all of Penn schools while being
-            culturally and ethnically diverse. It is our policy to have cross-collaboration across
-            these schools and build deeper relationships.
+            <b>We seek innovation.</b> Not only do we hope to engage schools but also institutions 
+            that cater entrepreneurship, sustainability, and technology. We aim to be the center of 
+            Penn's climate innovation scene, and we engage our members to do so.
+
           </li>
           <li>
-            <b>We seek innovation.</b> Not only do we hope to engage schools but also institutions
-            that cater entrepreneurship, sustainability, and technology. We aim to be the center of
-            the Penn Climate innovation scene and we engage our members to do so.
-          </li>
-          <li>
-            <b>We seek growth.</b> Our members' mental and physical welfare are of the utmost
-            importance to us. That being said, we want our members to grow in any avenue they see
-            lacking. We hope to put you in a position or working on a project where you can find the
-            most value from.
-          </li>
-          <li>
-            <b>Communication.</b> Feel free to voice your opinion. We run lean and nothing is really
-            set in stone. We find free communication vital to our growth.
+            <b>We seek growth.</b> We want our members to grow in any avenue they see a need in. 
+            We run lean with many opportunities for growth and leadership. We hope to put students 
+            in projects where they can find the most value from. We find free communication vital to our growth.
           </li>
         </ul>
 
