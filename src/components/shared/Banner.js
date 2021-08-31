812acc7e42143c8646157470f606138bef3d4302
hryoma <84463842+hryoma@users.noreply.github.com>
2021-08-31 09:34:32 -0400

    Develop (#27)
    * Added email contact for blog
    
    * Added links to linkedin
    
    * Updated fellowships content
    
    * Added application links

---
+++ src/components/shared/Banner.js
@@ -0,0 +1,13 @@
+import s from "styled-components";
+
+export const Banner = s.div`
+  background-color: #fff3cd;
+  border: 1px solid #ffeeba;
+  border-radius: 10px;
+  padding: 20px;
+  margin-bottom: 30px;
+  
+  & > p {
+    margin-bottom: 0;
+  }
+`
