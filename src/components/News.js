23448ee3a89b94280e1603f4d064082e164c842d
Daily Pennsylvanian Analytics <115905646+anmol-dash@users.noreply.github.com>
2024-09-29 11:13:40 -0400

    new board

--- src/components/News.js
+++ src/components/News.js
@@ -7,33 +7,69 @@ import Tile from "../ui/Tile"
 
 const articleItems = [
   {
-    imgLink: "https://snworksceo.imgix.net/dpn/9d04e1df-97c6-4dec-b9f3-0d16aa1ea004.sized-1000x1000.jpg?w=1000",
-    title: "Penn Climate Ventures hosts second annual competition ",
+    imgLink: "src/images/headshots/nicholas_kwok.png",
+    title: "Executive President, CAS '27",
+    publisher: "Nicholas Kwok",
+    date: "CAS '27"
+  },
+  {
+    imgLink: "src/images/headshots/nicholas_kwok.png",
+    title: "Co-President and Director of Operations",
+    publisher: "Daniel Lapinski",
+    date: "CAS '27"
+  },{
+    imgLink: "src/images/headshots/shaurya_singhi.png",
+    title: "Vice President",
+    publisher: "Shaurya Singhi",
+    date: "CAS '27"
+  },{
+    imgLink: "src/images/headshots/lukas_koch.png",
+    title: "Co-Director of Consulting",
+    publisher: "Lukas von Koch",
+    date: "Wharton '26"
+  },{
+    imgLink: "src/images/headshots/angela_ye.png",
+    title: "Co-Director of Consulting",
+    publisher: "Angela Ye",
+    date: "CAS '26"
+  },{
+    imgLink: "src/images/headshots/chloe_ng.png",
+    title: "Co-Director of Consulting",
+    publisher: "Chloe Ng",
+    date: "SEAS '26 CAS '26"
+  },{
+    imgLink: "src/images/headshots/sandro_mocciolo.png",
+    title: "Director of Policy",
+    publisher: "Sandro Mocciolo",
+    date: "CAS '27"
+  },{
+    imgLink: "src/images/headshots/emma_shockley.png",
+    title: "Director of Marketing",
+    publisher: "Emma Shockley",
+    date: "CAS '25"
+  },{
+    imgLink: "src/images/headshots/karam_alkaissy.png",
+    title: "Director of Operations",
+    publisher: "Karam Alkaissy",
+    date: "CAS '27"
+  },{
+    imgLink: "src/images/headshots/nicholas_kwok.png",
+    title: "Co-Director of Prize",
+    publisher: "Jeff Li",
+    date: "CAS '27"
+  },{
+    imgLink: "src/images/headshots/nicholas_kwok.png",
+    title: "ECo-Director of Prize",
+    publisher: "Brianna Hess",
+    date: "CAS '27"
+  },{
+    imgLink: "src/images/headshots/nicholas_kwok.png",
+    title: "Co-Director of Prize",
     link: "https://www.thedp.com/article/2022/03/climate-ventures-competition-prize-student-groups",
-    publisher: "Daily Pennsylvanian",
-    date: "Mar 28, 2022"
+    publisher: "Kana Unigame",
+    date: "CAS '27"
   },
-  {
-    imgLink: "https://snworksceo.imgix.net/dpn/17408c7b-dbab-4f87-8ede-db45b8933a46.sized-1000x1000.jpg?w=800",
-    title: "Penn Climate Ventures holds inaugural environmental tech startup competition",
-    link: "https://www.thedp.com/article/2021/04/penn-climate-ventures-impossible-foods",
-    publisher: "Daily Pennsylvanian",
-    date: "Apr 26, 2021"
-  },
-  {
-    imgLink: "https://static01.nyt.com/images/2021/02/24/opinion/24chatterjee/24chatterjee-facebookJumbo.jpg?year=2021&h=550&w=1050&s=3318cdf5414524ac4cfb0b6bd9f71fe4cdc13d13a77e290b369e4e2bf3181968&k=ZQJBKqZ0VN",
-    title: "How We Can Better Predict Weather Catastrophes",
-    link: "https://www.nytimes.com/2021/02/25/opinion/weather-forecast-climate-change.html",
-    publisher: "New York Times",
-    date: "Feb 25, 2021"
-  },
-  {
-    imgLink: "https://snworksceo.imgix.net/dpn/4ad60ade-ed53-436a-bb12-cd235b84bfd5.sized-1000x1000.jpg?w=800",
-    title: "New student group looks to design Penn course on business solutions to climate crisis",
-    link: "https://www.thedp.com/article/2021/01/penn-climate-ventures-innovation-environment-sustainable",
-    publisher: "Daily Pennsylvanian",
-    date: "Jan 29, 2021"
-  },
+  
 ]
 
 
@@ -41,12 +77,11 @@ export const News = () => {
   return (
     <>
       <CenteredSubtitle left={"left"}>In the News</CenteredSubtitle>
-      <GridContainer childWidth="300">
+      <GridContainer childWidth="200">
           { articleItems.map( article => (
             <Tile imgLink={article.imgLink}
                   topText={article.publisher}
                   midText={article.title}
-                  midLink={article.link}
                   botText={article.date} />
           )) }
       </GridContainer>
--- src/pages/collaborate.js
+++ src/pages/collaborate.js
@@ -9,20 +9,8 @@ import "../styles/base.scss"
 
 const CollaboratePage = () => {
   return (
-    <PageLayout title="Collaborate">
+    <PageLayout title="Meet The Board">
       <Container>
-        <Text bold>
-          Club Co-Sponsor, Speak/Mentor/Judge/Advise Interest, Corporate Sponsor Interest, Donate
-        </Text>
-        <Text>
-          If you or your group would like to support us, please contact us at <Email/> with a
-          statement of your area of interest and your involvement capacities.
-        </Text>
-        <Text>If you have press inquiries, please contact us at <Email/>.</Text>
-        <Text>
-          If you would like to submit a piece to the blog, please email{' '}
-          <UrlLink href={'mailto:preeths@sas.upenn.edu'}>preeths@sas.upenn.edu</UrlLink>.
-        </Text>
         <News />
       </Container>
     </PageLayout>
